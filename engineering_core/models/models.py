ffbe1b96ac017e7fc7564c3772be8cfa59bec00a
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-03 15:25:59 +0200

    Update service_type and area field labels

--- engineering_core/models/models.py
+++ engineering_core/models/models.py
@@ -8,12 +8,12 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     building_type = fields.Selection([('residential', 'سكن خاص'), ('investment', 'استثماري'), ('commercial', 'تجاري'), ('industrial', 'صناعي'), ('cooperative', 'جمعيات وتعاونيات'), ('mosque', 'مساجد'), ('hangar', 'مخازن / شبرات'), ('farm', 'مزارع')], string="نوع العقار", tracking=True)
-    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('extension', 'توسعة'), ('extension_modification', 'تعديل وتوسعة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة", tracking=True)
+    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('addition', 'اضافة'), ('addition_modification', 'تعديل واضافة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة", tracking=True)
     civil_number = fields.Char(string="الرقم المدني (Civil ID)")
     plot_no = fields.Char(string="رقم القسيمة (Plot)")
     block_no = fields.Char(string="القطعة (Block)")
     street_no = fields.Char(string="الشارع (Street)")
-    area = fields.Char(string="المساحة (Area)")
+    area = fields.Char(string="مساحة الارض (Area)")
 
 
 # ==============================================================================
@@ -23,11 +23,11 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     building_type = fields.Selection([('residential', 'سكن خاص'), ('investment', 'استثماري'), ('commercial', 'تجاري'), ('industrial', 'صناعي'), ('cooperative', 'جمعيات وتعاونيات'), ('mosque', 'مساجد'), ('hangar', 'مخازن / شبرات'), ('farm', 'مزارع')], string="نوع العقار")
-    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('extension', 'توسعة'), ('extension_modification', 'تعديل وتوسعة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة")
+    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('addition', 'اضافة'), ('addition_modification', 'تعديل واضافة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة")
     plot_no = fields.Char(string="رقم القسيمة (Plot)")
     block_no = fields.Char(string="القطعة (Block)")
     street_no = fields.Char(string="الشارع (Street)")
-    area = fields.Char(string="المساحة (Area)")
+    area = fields.Char(string="مساحة الارض (Area)")
     
     # This function copies the data to the new Quotation
     def _prepare_sale_order_values(self, partner, company, access_token):
@@ -52,9 +52,9 @@ class SaleOrder(models.Model):
     # The fields are now fully independent and editable.
     
     building_type = fields.Selection([('residential', 'سكن خاص'), ('investment', 'استثماري'), ('commercial', 'تجاري'), ('industrial', 'صناعي'), ('cooperative', 'جمعيات وتعاونيات'), ('mosque', 'مساجد'), ('hangar', 'مخازن / شبرات'), ('farm', 'مزارع')], string="نوع العقار", store=True)
-    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('extension', 'توسعة'), ('extension_modification', 'تعديل وتوسعة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة", store=True)
+    service_type = fields.Selection([('new_construction', 'بناء جديد'), ('demolition', 'هدم'), ('modification', 'تعديل'), ('addition', 'اضافة'), ('addition_modification', 'تعديل واضافة'), ('supervision_only', 'إشراف هندسي فقط'), ('renovation', 'ترميم'), ('internal_partitions', 'قواطع داخلية'), ('shades_garden', 'مظلات / حدائق')], string="نوع الخدمة", store=True)
 
     plot_no = fields.Char(string="رقم القسيمة", store=True)
     block_no = fields.Char(string="القطعة", store=True)
     street_no = fields.Char(string="الشارع", store=True)
-    area = fields.Char(string="المساحة", store=True)
+    area = fields.Char(string="مساحة الارض", store=True)
