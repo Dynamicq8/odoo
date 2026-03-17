aee22e7155bf34b1057a508640ffd8e707a9fee3
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-17 02:26:11 +0200

    Create __manifest__.py

---
+++ engineering_sign_template_renamer/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Sign Template Renamer",
+    'version': '1.0',
+    'category': 'Sign',
+    'summary': 'Helper to rename sign template fields when UI is buggy.',
+    'depends': ['sign'], # Depends on the sign module
+    'data': [
+        'views/sign_template_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
