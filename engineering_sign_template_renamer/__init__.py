4708317716072b391a0674abdfee91167443aabf
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-17 02:29:23 +0200

    Create __init__.py

---
+++ engineering_sign_template_renamer/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import models
+# If you add other sub-directories like 'wizards' or 'controllers' later,
+# you would add them here as well:
+# from . import wizards
+# from . import controllers
