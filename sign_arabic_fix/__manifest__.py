fd7d44db2f12f4dd67e056e7b0f30231441ffefb
mazen41 <161659553+mazen41@users.noreply.github.com>
2026-03-21 10:45:52 +0200

    Create __manifest__.py

---
+++ sign_arabic_fix/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Sign Arabic Fix',
+    'version': '17.0.1.0',
+    'category': 'Sign',
+    'summary': 'Fixes Arabic text rendering (Right-to-Left) in Sign PDF',
+    'depends': ['sign'],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
