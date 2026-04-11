844a7e0c70829f0be4a3baa1ae6281ecb71dc0b6
mazen41 <mazehossny@gmail.com>
2026-04-11 16:10:20 +0200

    message

--- engineering_commitments/models/__init__.py
+++ engineering_commitments/models/__init__.py
@@ -3,4 +3,5 @@ from . import sign_template # assuming this file exists and contains sign_templa
 from . import project_task   # This file now contains all project/task extensions AND the new contract models
 from . import engineering_task_commitment # These are your *existing* commitment models
 from . import engineering_project_commitment # These are your *existing* commitment models
+from . import res_company # These are your *existing* commitment models
 # REMOVE: from . import engineering_project_contract (as it doesn't exist as a separate file and isn't needed)
